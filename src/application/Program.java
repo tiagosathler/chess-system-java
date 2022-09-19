60c21a40f82b0a44740e7ab5d9ff98f58d31d682
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-09-19 12:23:44 -0300

    :sparkles: feat: create Board and Piece Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Board board = new Board(8,8);
 	}
 
 }
