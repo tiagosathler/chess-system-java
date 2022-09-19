b68f62a2da324d8832860bbd8db470f00c4593d3
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-09-19 16:11:11 -0300

    :sparkles: feat: Rook and King

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		UI.printBoard(chessMatch.getPieces());		
 	}
 
 }
